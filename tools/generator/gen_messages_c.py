dc7754f89af284cc940e7f0a890cb5a96475ab07
Felix Ruess <felix.ruess@roboception.de>
2016-04-01 12:27:05 +0200

    only enable file descriptor functionality if PPRZLINK_ENABLE_FD
    if turned off this saves significant space in ROM, so we can support the small boards

--- tools/generator/gen_messages_c.py
+++ tools/generator/gen_messages_c.py
@@ -42,6 +42,18 @@ extern "C" {
 #include "pprzlink/pprzlink_transport.h"
 #include "pprzlink/pprzlink_utils.h"
 
+#ifndef PPRZLINK_ENABLE_FD
+#define PPRZLINK_ENABLE_FD FALSE
+#endif
+
+// dummy fd to save ROM if this is not used
+#if !PPRZLINK_ENABLE_FD
+#define _FD 0
+#define _FD_ADDR 0
+#else
+#define _FD_ADDR &_FD
+#endif
+
 #if DOWNLINK
 ${{message:#define DL_${msg_name} ${id}
 #define PPRZ_MSG_ID_${msg_name} ${id}
@@ -51,14 +63,16 @@ ${{message:#define DL_${msg_name} ${id}
 ${{message:
 #define DOWNLINK_SEND_${msg_name}(_trans, _dev${{fields:, ${attrib_macro}}}) pprz_msg_send_${msg_name}(&((_trans).trans_tx), &((_dev).device), AC_ID${{fields:, ${attrib_macro}}})
 static inline void pprz_msg_send_${msg_name}(struct transport_tx *trans, struct link_device *dev, uint8_t ac_id${{fields:, ${attrib_fun}}}) {
-  long fd = 0; /* can be an address, an index, a file descriptor, ... */
-  if (trans->check_available_space(trans->impl, dev, &fd, trans->size_of(trans->impl, 0${{fields:${array_extra_length}+${length}}} +2 /* msg header overhead */))) {
+#if PPRZLINK_ENABLE_FD
+  long _FD = 0; /* can be an address, an index, a file descriptor, ... */
+#endif
+  if (trans->check_available_space(trans->impl, dev, _FD_ADDR, trans->size_of(trans->impl, 0${{fields:${array_extra_length}+${length}}} +2 /* msg header overhead */))) {
     trans->count_bytes(trans->impl, dev, trans->size_of(trans->impl, 0${{fields:${array_extra_length}+${length}}} +2 /* msg header overhead */));
-    trans->start_message(trans->impl, dev, fd, 0${{fields:${array_extra_length}+${length}}} +2 /* msg header overhead */);
-    trans->put_bytes(trans->impl, dev, fd, DL_TYPE_UINT8, DL_FORMAT_SCALAR, &ac_id, 1);
-    trans->put_named_byte(trans->impl, dev, fd, DL_TYPE_UINT8, DL_FORMAT_SCALAR, DL_${msg_name}, "${msg_name}");
-    ${{fields:${array_byte}trans->put_bytes(trans->impl, dev, fd, DL_TYPE_${type_upper}, ${dl_format}, (void *) _${field_name}, ${length});
-    }}trans->end_message(trans->impl, dev, fd);
+    trans->start_message(trans->impl, dev, _FD, 0${{fields:${array_extra_length}+${length}}} +2 /* msg header overhead */);
+    trans->put_bytes(trans->impl, dev, _FD, DL_TYPE_UINT8, DL_FORMAT_SCALAR, &ac_id, 1);
+    trans->put_named_byte(trans->impl, dev, _FD, DL_TYPE_UINT8, DL_FORMAT_SCALAR, DL_${msg_name}, "${msg_name}");
+    ${{fields:${array_byte}trans->put_bytes(trans->impl, dev, _FD, DL_TYPE_${type_upper}, ${dl_format}, (void *) _${field_name}, ${length});
+    }}trans->end_message(trans->impl, dev, _FD);
   } else
     trans->overrun(trans->impl, dev);
 }
@@ -105,7 +119,7 @@ def generate(output, xml):
 
     directory, name = os.path.split(output)
 
-    
+
     print("Generating C implementation in %s" % output)
     if directory != '':
         pprz_parse.mkdir_p(directory)
@@ -118,7 +132,7 @@ def generate(output, xml):
                 f.attrib_macro = 'nb_%s, %s' % (f.field_name, f.field_name)
                 f.attrib_fun = 'uint8_t nb_%s, %s *_%s' % (f.field_name, f.type, f.field_name)
                 f.attrib_fun_unused = 'uint8_t nb_%s __attribute__((unused)), %s *_%s __attribute__((unused))' % (f.field_name, f.type, f.field_name)
-                f.array_byte = 'trans->put_bytes(trans->impl, dev, fd, DL_TYPE_ARRAY_LENGTH, DL_FORMAT_SCALAR, (void *) &nb_%s, 1);\n    ' % f.field_name
+                f.array_byte = 'trans->put_bytes(trans->impl, dev, _FD, DL_TYPE_ARRAY_LENGTH, DL_FORMAT_SCALAR, (void *) &nb_%s, 1);\n    ' % f.field_name
                 f.read_type = f.type+'_array'
                 if (offset + 1) % min(4, int(f.type_length)) == 0: # data are aligned
                     f.read_array_byte = '#define DL_%s_%s_length(_payload) _PPRZ_VAL_uint8_t(_payload, %d)\n' % (m.msg_name, f.field_name, offset)
@@ -154,4 +168,3 @@ def generate(output, xml):
 
     generate_messages_h(directory, name, xml)
     copy_fixed_headers(directory, xml.protocol_version)
-
