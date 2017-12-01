6f88180ad79fa69809746e716fa799bc164c7b9a
Michal Podhradsky <mpodhradsky@galois.com>
2017-11-30 17:00:29 -0800

    First step

--- tools/generator/gen_messages_v1_0_c.py
+++ tools/generator/gen_messages_v1_0_c.py
@@ -10,22 +10,10 @@ based on:
     Released under GNU GPL version 3 or later
 '''
 
-import sys, os
-import gen_messages, pprz_template, pprz_parse
-
-t = pprz_template.PPRZTemplate()
-
-def generate_messages_h(directory, name, xml):
-    print(xml)
-    '''generate messages header'''
-    if name == 'stdout':
-        f = sys.stdout
-    else:
-        f = open(os.path.join(directory, name), mode='w')
-    t.write(f,'''
+'''
 /** @file
- *	@brief PPRZLink messages header built from ${filename}
- *	@see http://paparazziuav.org
+ *    @brief PPRZLink messages header built from ${filename}
+ *    @see http://paparazziuav.org
  */
 #ifndef _VAR_MESSAGES_${class_name}_H_
 #define _VAR_MESSAGES_${class_name}_H_
@@ -93,6 +81,47 @@ ${{message:${{fields:${read_array_byte}#define DL_${msg_name}_${field_name}(_pay
 #endif // __cplusplus
 
 #endif // _VAR_MESSAGES_${class_name}_H_
+'''
+import re
+import sys, os
+import gen_messages, pprz_template, pprz_parse
+
+t = pprz_template.PPRZTemplate()
+
+def generate_messages_h(directory, name, xml):
+    print(xml)
+    '''generate messages header'''
+    if name == 'stdout':
+        f = sys.stdout
+    else:
+        f = open(os.path.join(directory, name), mode='w')
+    t.write(f,'''
+/*
+ *
+ *
+ * Distributed under the OpenDDS License.
+ * See: http://www.opendds.org/license.html
+ */
+
+  typedef sequence<unsigned short> unsigned_short_array; // uint8 ot uint16[]
+  typedef sequence<short> short_array; // int8 or int16[]
+  typedef sequence<unsigned long> unsigned_long_array; // uint32[]
+  typedef sequence<long> long_array; // int32[]
+  typedef sequence<float> float_array; // float[]
+  typedef sequence<double> double_array; // double[]
+
+module ${class_name} {
+${{message:
+#pragma DCPS_DATA_TYPE "${class_name}::${msg_name}"
+#pragma DCPS_DATA_TYPE "${class_name}::${msg_name} msg_id"
+
+struct ${msg_name} {
+    short msg_id;
+${{fields:    ${opendds_type} field_${attrib_macro}}}
+    };
+}}
+
+};
 ''', xml)
     if name != 'stdout':
         f.close()
@@ -126,11 +155,37 @@ def generate(output, xml):
     print("Generating C implementation in %s" % output)
     if directory != '':
         pprz_parse.mkdir_p(directory)
+    
+    # fix the class name to conform with standards
+    xml.class_name = xml.class_name.title() 
 
     # add some extra field attributes for convenience with arrays
     for m in xml.message:
+        m.msg_name = m.msg_name.title()
+        m.msg_name = re.sub('[_]', '', m.msg_name) 
         offset = 2 # 2 bytes initial offset (sender id, message id)
         for f in m.fields:
+            if f.type == 'uint8_t' or f.type == 'uint16_t' or f.type == 'char':
+                f.opendds_type = 'unsigned short'
+            elif f.type == 'int8_t' or f.type == 'int16_t':
+                f.opendds_type = 'short'
+            elif f.type == 'uint32_t':
+                f.opendds_type = 'unsigned long'
+            elif f.type == 'int32_t':
+                f.opendds_type = 'long'
+            elif f.type == 'float':
+                f.opendds_type = 'float'
+            elif f.type == 'double':
+                f.opendds_type = 'double'
+            else:
+                f.opendds_type = 'void'
+            
+            if not type(f.array_type) == type(None):
+                # it is an array - wrap as a stream
+                f.opendds_type = re.sub('[ ]', '_', f.opendds_type) + '_array'
+
+            f.attrib_macro = '%s;\n' % f.field_name
+            '''
             if f.array_type == 'VariableArray':
                 f.attrib_macro = 'nb_%s, %s' % (f.field_name, f.field_name)
                 f.attrib_fun = 'uint8_t nb_%s, %s *_%s' % (f.field_name, f.type, f.field_name)
@@ -161,13 +216,14 @@ def generate(output, xml):
             else:
                 f.offset = offset
                 offset += int(f.length)
-                f.attrib_macro = '%s' % f.field_name
+                f.attrib_macro = '%s;\n' % f.field_name
                 f.attrib_fun = '%s *_%s' % (f.type, f.field_name)
                 f.attrib_fun_unused = '%s *_%s __attribute__((unused))' % (f.type, f.field_name)
                 f.array_byte = ''
                 f.read_type = f.type
                 f.read_array_byte = ''
                 f.dl_format = 'DL_FORMAT_SCALAR'
+            '''
 
     generate_messages_h(directory, name, xml)
     copy_fixed_headers(directory, xml.protocol_version)
